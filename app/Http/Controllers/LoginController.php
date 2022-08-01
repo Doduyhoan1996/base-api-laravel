2eaf9966bd3430e8f80ef987b2464818d03d9c2c
quanghuy03092017 <quanghuy03092017@gmail.com>
2022-08-01 11:43:01 +0700

    refactor code

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application as Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class LoginController extends Controller
+{
+    public function showLoginForm(): Factory|View|Application
+    {
+        return view('login');
+    }
+}
